010af453cb4a3f40b3f6f123193723ccf169861c
Dalv-Dalv <futurevlad2@gmail.com>
2025-12-29 14:13:46 +0200

    Dont allocate on heap in main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include "Implementations/ThreeBodyGL.h"
 
 int main() {
-	auto* glslCompute = new ThreeBodyGL(1920, 1080, true);
-	glslCompute->Animate(1920, 1080);
-	delete glslCompute;
+	auto glslCompute = ThreeBodyGL(1920, 1080, true);
+	glslCompute.Animate(1920, 1080);
 	return 0;
 }
